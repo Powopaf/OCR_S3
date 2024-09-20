a5236ac721104358999f62654b58e754ba50a8b5
Powopaf <paf.fournier@gmail.com>
2024-09-20 19:17:05 +0200

    add sdl util

---
+++ OCR/src/Pretreatment/setup_sdl.c
@@ -0,0 +1,23 @@
+#include <SDL2/SDL.h>
+#include <err.h>
+void sdl_setup() {
+    // we can add arguements if needed
+    if(SDL_Init(SDL_INIT_VIDEO)) {
+        errx(EXIT_FAILURE, "fail to init sdl_video: %s", SDL_GetError());
+    }
+}
+
+void sdl_close() {
+    // we can add arguements if needed
+    SDL_Quit();
+}
+
+void drawline(SDL_Renderer *renderer, 
+        int x1, int y1, int x2, int y2, Uint8 r, Uint8 g, Uint8 b) {
+    /*
+     * x1,y1,x2,y2 -> coordinate to draw between
+     * r,g,b -> the color of the line
+     */
+    SDL_SetRenderDrawColor(renderer, r, g, b, 255);
+    SDL_RenderDrawline(renderer, x1, y1, x2, y2);
+}
