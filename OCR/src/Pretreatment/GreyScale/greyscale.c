fcbf5be923fab359947bbbec401a5c0edac7f3c7
PAF <123662077+Powopaf@users.noreply.github.com>
2024-09-22 16:14:44 +0200

    Sdl test (#12)
    Co-authored-by: Corentin del Pozo <darkrentin@gmail.com>

---
+++ OCR/src/Pretreatment/GreyScale/greyscale.c
@@ -0,0 +1,40 @@
+#include <SDL2/SDL.h>
+#include <err.h>
+///////////////////////////
+// to run greyscale only //
+//////////////////////////
+#include <SDL2/SDL_image.h>
+#include "../Utils/convert.h"
+#include "../Utils/sdl_utils.h"
+////////////////////////////////
+
+
+void greyscale(SDL_Surface *surface) {
+    SDL_PixelFormat* format = surface->format;
+    int width = surface->w;
+    int height = surface->h;
+    int p = surface->pitch;
+    int bpp = format->BytesPerPixel;
+    Uint8* pix = (Uint8*)surface->pixels;
+    SDL_LockSurface(surface);
+    for(int j = 0; j < height; j++) {
+        for(int i = 0; i < width; i++) {
+            Uint8* pixel = pix + j * p + i * bpp;
+            Uint8 grey = 0.212671f * pixel[0] + 0.715160f * pixel[1] + 0.072169f * pixel[2];
+            pixel[0] = grey;
+            pixel[1] = grey;
+            pixel[2] = grey;
+        }
+    }
+    SDL_UnlockSurface(surface);
+}
+//remove to run project un comment to test greyscale
+int main(int argc, char* argv[]) {
+    convert(argv[1]);
+    sdl_setup();
+    SDL_Surface* surface = SDL_LoadBMP("img.bmp"); //convert() create a img.bmp
+    greyscale(surface);
+    SDL_SaveBMP(surface, "img.bmp");
+    SDL_FreeSurface(surface);
+    sdl_close();
+}
--- OCR/src/Pretreatment/Utils/convert.c
+++ OCR/src/Pretreatment/Utils/convert.c
@@ -1,11 +1,11 @@
-#include "convert.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 void convert(const char* path){	//path MUST end with the impg.png file
 	char prefix[] = "convert ";	//part before the path in command
-	char suffix[] = " -alpha off img.bmp";	//part after the path in command
+	char suffix[] = " img.bmp";	//part after the path in command can add -alpha
+                                //off
     // Use sizeof to allow for null char at end.
     char *convert = malloc(sizeof(prefix) + strlen(path) + sizeof(suffix));
     if (convert != NULL){
