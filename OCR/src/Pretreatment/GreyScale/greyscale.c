97d63647661fad77d530c2b75c7622811cabf8ec
Powopaf <paf.fournier@gmail.com>
2024-09-23 14:15:57 +0200

    comment

--- OCR/src/Pretreatment/GreyScale/greyscale.c
+++ OCR/src/Pretreatment/GreyScale/greyscale.c
@@ -3,9 +3,8 @@
 ///////////////////////////
 // to run greyscale only //
 //////////////////////////
-#include <SDL2/SDL_image.h>
-#include "../Utils/convert.h"
-#include "../Utils/sdl_utils.h"
+//#include "../Utils/convert.h"
+//#include "../Utils/sdl_utils.h"
 ////////////////////////////////
 
 
@@ -19,7 +18,7 @@ void greyscale(SDL_Surface *surface) {
     SDL_LockSurface(surface);
     for(int j = 0; j < height; j++) {
         for(int i = 0; i < width; i++) {
-            Uint8* pixel = pix + j * p + i * bpp;
+            Uint8* pixel = pix + j * p + i * bpp; // magic line
             Uint8 grey = 0.212671f * pixel[0] + 0.715160f * pixel[1] + 0.072169f * pixel[2];
             pixel[0] = grey;
             pixel[1] = grey;
@@ -29,7 +28,7 @@ void greyscale(SDL_Surface *surface) {
     SDL_UnlockSurface(surface);
 }
 //comment to run project uncomment to test greyscale
-int main(int argc, char* argv[]) {
+/*int main(int argc, char* argv[]) {
     convert(argv[1]);
     sdl_setup();
     SDL_Surface* surface = SDL_LoadBMP("img.bmp"); //convert() create a img.bmp
@@ -37,4 +36,4 @@ int main(int argc, char* argv[]) {
     SDL_SaveBMP(surface, "img.bmp");
     SDL_FreeSurface(surface);
     sdl_close();
-}
+}*/
