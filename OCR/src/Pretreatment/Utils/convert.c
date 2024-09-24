8ca40cb4a4427c69b70676d0e3155cd5e9513054
Powopaf <paf.fournier@gmail.com>
2024-09-24 20:21:48 +0200

    init

--- OCR/src/Pretreatment/Utils/convert.c
+++ OCR/src/Pretreatment/Utils/convert.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 void convert(const char* path){	//path MUST end with the impg.png file
-	char prefix[] = "convert ";	//part before the path in command
+    char prefix[] = "convert ";	//part before the path in command
 	char suffix[] = " img.bmp";	//part after the path in command can add -alpha
                                 //off
     // Use sizeof to allow for null char at end.
