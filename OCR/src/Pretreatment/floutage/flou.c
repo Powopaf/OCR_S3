81ab6010e7361a45f583daf2ddc85e7d4fc343de
Powopaf <paf.fournier@gmail.com>
2024-09-26 20:21:46 +0200

    init blurring function

---
+++ OCR/src/Pretreatment/floutage/flou.c
@@ -0,0 +1,10 @@
+#include <SDL2/SDL.h>
+
+void blurring(SDL_Surface* surface) {
+    //FIXME
+}
+
+//comment to run project uncomment to run blurring()
+int main(int argc, char* argv[]) {
+    return EXIT_SUCCESS;
+}
