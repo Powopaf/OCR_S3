5aac1d057531e14620f8cde3cc9da6672c90652e
Powopaf <paf.fournier@gmail.com>
2024-09-28 10:40:25 +0200

    ToDO fix blur

--- OCR/src/Pretreatment/blur/blur.c
+++ OCR/src/Pretreatment/blur/blur.c
@@ -3,8 +3,9 @@
 
 //comment this include to run project uncomment to run blur.c
 #include "../Utils/convert.h"
+#include "../Utils/sdl_utils.h"
+#include "../GreyScale/greyscale.h"
 //////////////////////////////////////////////////////////////
-
 
 void blurring(SDL_Surface* surface) {
     /*
@@ -22,17 +23,17 @@ void blurring(SDL_Surface* surface) {
             int x = i * p;
             int y = j * bpp;
             Uint8* pixel = pix + x + y;
-            Uint8* side_pixels[16] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+            Uint8 side_pixels[16] = { 0, 0, 0, 0, 0, 0, 0, 0 };
             //to access the pixel aroud the current
-            side_pixels[0] = pixel - width * bpp - 1;   // 0 1 2
-            side_pixels[1] = pixel - width * bpp;       // 3 c 4
-            side_pixels[2] = pixel - width * bpp + 3;   // 5 8 7
-            side_pixels[3] = pixel - 1;
-            side_pixels[4] = pixel + 3;
-            side_pixels[5] = pixel + width * bpp - 3;
-            side_pixels[6] = pixel + width * bpp;
-            side_pixels[7] = pixel + width * bpp + 1;
-            Uint8* moy = 0;
+            side_pixels[0] =*(pixel - width * bpp - 1);   // 0 1 2
+            side_pixels[1] =*(pixel - width * bpp);       // 3 c 4
+            side_pixels[2] = *(pixel - width * bpp + 3);   // 5 8 7
+            side_pixels[3] = *(pixel - 1);
+            side_pixels[4] = *(pixel + 3);
+            side_pixels[5] = *(pixel + width * bpp - 3);
+            side_pixels[6] = (*pixel + width * bpp);
+            side_pixels[7] = (*pixel + width * bpp + 1);
+            Uint8 moy = 0;
             for (size_t i = 0; i < 8; i++) {
                 moy = moy + side_pixels[i];
             }
@@ -49,5 +50,12 @@ void blurring(SDL_Surface* surface) {
 
 //comment to run project uncomment to run blurring()
 int main(int argc, char* argv[]) {
+    convert(argv[1]);
+    sdl_setup();
+    SDL_Surface* surface = SDL_LoadBMP("img.bmp");
+    //greyscale(surface);
+    SDL_SaveBMP(surface, "blur.bmp");
+    SDL_FreeSurface(surface);
+    sdl_close();
     return EXIT_SUCCESS;
 }
