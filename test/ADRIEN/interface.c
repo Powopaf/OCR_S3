c834204bb38eb92f090e433b30478054d9c850f9
adrien <adrien.coureau@epita.fr>
2024-10-04 22:18:54 +0200

    glade progress, new interface

--- test/ADRIEN/interface.c
+++ test/ADRIEN/interface.c
@@ -3,6 +3,8 @@
 //#include <stdlib.h>
 #include <gtk/gtk.h>
 
+char *image_file_path = NULL;
+
 // Make objects global variables
 GtkBuilder *builder;
 GtkWidget *window;
@@ -41,6 +43,7 @@ int main(int argc, char *argv[])
     return EXIT_SUCCESS;
 }
 
+/*
 void on_button_import_clicked(GtkButton *b)
 {
     gtk_label_set_text(GTK_LABEL(label_import),(const gchar*)
@@ -60,4 +63,60 @@ void on_button_import_clicked(GtkButton *b)
     //gchar *filename = gtk_file_chooser_get_current_name(button_import);
     gtk_image_set_from_file(GTK_IMAGE(image),filename);
     g_free(filename);
+}
+*/
+
+
+void on_button_import_clicked(GtkButton *button) {
+    GtkWidget *dialog;
+    GtkFileChooser *chooser;
+    GtkFileFilter *filter;
+
+    // Use the main window as the parent for the dialog
+    GtkWidget *parent_window = gtk_widget_get_toplevel(GTK_WIDGET(button));
+
+    // Create a file chooser dialog
+    dialog = gtk_file_chooser_dialog_new("Choose an image", GTK_WINDOW(parent_window),
+                                         GTK_FILE_CHOOSER_ACTION_OPEN, 
+                                         "_Cancel", GTK_RESPONSE_CANCEL, 
+                                         "_Open", GTK_RESPONSE_ACCEPT, NULL);
+    
+    // Check if dialog is created
+    if (dialog == NULL) 
+    {
+        g_print("Failed to create dialog\n");
+        return;
+    }
+
+    chooser = GTK_FILE_CHOOSER(dialog);
+
+    // Add file filters
+    filter = gtk_file_filter_new();
+    gtk_file_filter_add_pattern(filter, "*.jpg");
+    gtk_file_filter_add_pattern(filter, "*.jpeg");
+    gtk_file_filter_add_pattern(filter, "*.png");
+    gtk_file_filter_add_pattern(filter, "*.bmp");
+    gtk_file_chooser_add_filter(chooser, filter);
+
+    // Show the dialog and wait for a response
+    int response = gtk_dialog_run(GTK_DIALOG(dialog));
+    if (response == GTK_RESPONSE_ACCEPT) {
+        // Free the previous file path, if any
+        if (image_file_path) 
+        {
+            g_free(image_file_path);
+        }
+
+        image_file_path = gtk_file_chooser_get_filename(chooser);
+        g_print("Selected file: %s\n", image_file_path);  // Debug output
+        gtk_button_set_label(button, image_file_path);
+        load_image(image_file_path);
+    } 
+    else 
+    {
+        g_print("Dialog closed without selecting a file\n");  // Debug output
+    }
+
+    // Destroy the dialog
+    gtk_widget_destroy(dialog);
 }
