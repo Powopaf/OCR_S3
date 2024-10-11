44b9cf61b6a1f93d81e009f5db577a0548da3289
Powopaf <paf.fournier@gmail.com>
2024-10-11 18:22:25 +0200

    finish contrast

--- OCR/src/Pretreatment/Contrast/contrast.c
+++ OCR/src/Pretreatment/Contrast/contrast.c
@@ -7,26 +7,30 @@
 
 
 void contrast(SDL_Surface* surface) {
-    const Uint8 max_intensity = 0;
-    const Uint8 min_intensity = 255;
+    //const Uint8 max_intensity = 0;
+    //const Uint8 min_intensity = 255;
     int width = surface->w;
     int height = surface->h;
     int bpp = surface->format->BytesPerPixel;
     SDL_LockSurface(surface);
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
+    for (int i = 1; i < height - 1; i++) {
+        for (int j = 1; j < width - 1; j++) {
             //current pixel
             Uint8* pixel = surface->pixels + i * surface->pitch + j * bpp;
-            /*Uint8* side_pixel[8];
-            side_pixels[0] = *(pixel - width * bpp - 3);    // 0 1 2
-            side_pixels[1] = *(pixel - width * bpp);        // 3 c 4
-            side_pixels[2] = *(pixel - width * bpp + 3);    // 5 6 7
-            side_pixels[3] = *(pixel - 3);                  // [11,12,13] [21,22,23] [31,32,33]
-            side_pixels[4] = *(pixel + 3);                  // [11,12,13] [21,22,23] [31,32,33]
-            side_pixels[5] = *(pixel + width * bpp - 3);    // [11,12,13] [21,22,23] [31,32,33]
-            side_pixels[6] = *(pixel + width * bpp);
-            side_pixels[7] = *(pixel + width * bpp + 3);*/
-            pixel[2] = 255 * ((pixel[0] - min_intensity) / (max_intensity - min_intensity));
+            Uint8 side_pixel[8];
+            side_pixel[0] = *(pixel - width * bpp - 3);    // 0 1 2
+            side_pixel[1] = *(pixel - width * bpp);        // 3 c 4
+            side_pixel[2] = *(pixel - width * bpp + 3);    // 5 6 7
+            side_pixel[3] = *(pixel - 3);                  // [11,12,13] [21,22,23] [31,32,33]
+            side_pixel[4] = *(pixel + 3);                  // [11,12,13] [21,22,23] [31,32,33]
+            side_pixel[5] = *(pixel + width * bpp - 3);    // [11,12,13] [21,22,23] [31,32,33]
+            side_pixel[6] = *(pixel + width * bpp);
+            side_pixel[7] = *(pixel + width * bpp + 3);
+            for (size_t k = 0; k < 8; k++) {
+                if (side_pixel[k] < pixel[0] && pixel[0] < 200) {
+                    pixel[2] = pixel[2] - 50 < 0 ? 0 : pixel[2] - 50;
+                }
+            }
         }
     }
     for (int i = 0; i < height; i++) {
@@ -41,6 +45,17 @@ void contrast(SDL_Surface* surface) {
 
 
 //comment to run project, uncomment to run contrast
-int main(int argc, char* argv) {
+int main(int argc, char* argv[]) {
+    convert(argv[1]);
+    sdl_setup();
+    SDL_Surface* s = SDL_LoadBMP("img.bmp");
+    greyscale(s);
+    SDL_SaveBMP(s, "c.bmp");
+    SDL_Surface* s1 = SDL_LoadBMP("c.bmp");
+    contrast(s1);
+    SDL_SaveBMP(s1, "c.bmp");
+    SDL_FreeSurface(s1);
+    SDL_FreeSurface(s);
+    sdl_close();
     return EXIT_SUCCESS;
 }
