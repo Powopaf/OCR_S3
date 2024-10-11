cb4fad32a313a96ac4ad5b4a97303e7065b40849
Powopaf <paf.fournier@gmail.com>
2024-10-11 13:25:36 +0200

    contrast

---
+++ OCR/src/Pretreatment/Contrast/contrast.c
@@ -0,0 +1,16 @@
+#include <SDL2/SDL.h>
+
+//comment to run project, uncomment to run contrast
+#include "../Utils/convert.h"
+#include "../Utils/sdl_utils.h"
+#include "../GreyScale/greyscale.h"
+
+void contrast(SDL_Surface* surface) {
+    
+}
+
+
+//comment to run project, uncomment to run contrast
+int main(int argc, char* argv) {
+    return EXIT_SUCCESS;
+}
