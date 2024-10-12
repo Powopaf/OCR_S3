01b38e1c0290f9bad99ffc01c8f5c39fb90d3de9
Sophie <sophielou@HP202403>
2024-10-13 00:05:10 +0200

    Rotation3: The Best one, with a final image centred and a best pixels conservation

--- OCR/src/Pretreatment/rotation/rotate.c
+++ OCR/src/Pretreatment/rotation/rotate.c
@@ -5,6 +5,54 @@
 #include "../Utils/sdl_utils.h"
 ////////////
 
+// Rotation by rotation matrix (Best result)
+SDL_Surface* rotation3(SDL_Surface* image, double angle)
+{
+    int width = image->w;
+    int height = image->h;
+    int bpp = image->format->BytesPerPixel;
+    int p = image->pitch;
+    Uint8* pixels = (Uint8*)image->pixels;
+
+    double angle_radian = angle * M_PI / 180.0;
+
+    double cos_angle = cos(angle_radian);
+    double sin_angle = sin(angle_radian);
+
+    int new_width = width * 2;
+    int new_height = height * 2;
+
+    SDL_Surface* rotated_image = SDL_CreateRGBSurfaceWithFormat(0, new_width, new_height, 24, SDL_PIXELFORMAT_RGB24);
+    Uint8* rotated_pixels = (Uint8*)rotated_image->pixels;
+
+    int center_x = width / 2;
+    int center_y = height / 2;
+    int new_center_x = new_width / 2;
+    int new_center_y = new_height / 2;
+
+    for (int y = 0; y < new_height; y++) 
+    {
+        for (int x = 0; x < new_width; x++) 
+        {
+            int old_x = (int)((x - new_center_x) * cos_angle + (y - new_center_y) * sin_angle) + center_x;
+            int old_y = (int)(-(x - new_center_x) * sin_angle + (y - new_center_y) * cos_angle) + center_y;
+
+            if (old_x >= 0 && old_x < width && old_y >= 0 && old_y < height)
+            {
+                Uint8* original_pixel = pixels + (old_y * p) + (old_x * bpp);
+
+                Uint8* rotated_pixel = rotated_pixels + (y * rotated_image->pitch) + (x * rotated_image->format->BytesPerPixel);
+                rotated_pixel[0] = original_pixel[0];
+                rotated_pixel[1] = original_pixel[1];
+                rotated_pixel[2] = original_pixel[2];
+            }
+        }
+    }
+
+    return rotated_image;
+}
+
+// Rotation by shearing
 SDL_Surface *rotation2(SDL_Surface* image, double angle)
 {
         int width = image->w;
@@ -12,7 +60,8 @@ SDL_Surface *rotation2(SDL_Surface* image, double angle)
 	Uint8* pix = image->pixels;
 	int p = image->pitch;
 	int bpp = image->format->BytesPerPixel;
-
+	
+	// Convert angle to radian
         double angle_radian = angle * (M_PI / 180.0);
         int center_x = width / 2;
         int center_y = height / 2;
@@ -40,6 +89,8 @@ SDL_Surface *rotation2(SDL_Surface* image, double angle)
 
 			new_y += center_y;
 			new_x += center_x;
+			
+                        // Checks if the position is within the bounds of the original image
                         if (1 || (0 <= new_x && new_x < width && 0 <= new_y && new_y < height))
                         {
 				Uint8* newp = pix_r + (new_x+center_x/2) * protate + (new_y+center_y/2) * rotated_image->format->BytesPerPixel;
@@ -53,7 +104,7 @@ SDL_Surface *rotation2(SDL_Surface* image, double angle)
 }
 
 
-// Rotate an image
+// Rotate an image with loss of pixels
 SDL_Surface *rotation(SDL_Surface* image, double angle)
 {
         int width = image->w;
@@ -99,7 +150,7 @@ int main(int argc, char* argv[]) {
 	convert(argv[1]);
     	sdl_setup();
     	SDL_Surface* surface = SDL_LoadBMP("img.bmp"); //convert() create a img.bmp
-    	SDL_Surface* a = rotation2(surface, 16.0);
+    	SDL_Surface* a = rotation3(surface, 450.0);
     	SDL_SaveBMP(a, "img.bmp");
     	SDL_FreeSurface(surface);
 	SDL_FreeSurface(a);
