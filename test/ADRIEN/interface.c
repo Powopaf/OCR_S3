e15a28b967ef44df8e525e549e349cb7dfe4cb4b
adrien <adrien.coureau@epita.fr>
2024-10-14 20:44:36 +0200

    interface.c file progress, object init done

--- test/ADRIEN/interface.c
+++ test/ADRIEN/interface.c
@@ -3,16 +3,44 @@
 //#include <stdlib.h>
 #include <gtk/gtk.h>
 
-char *image_file_path = NULL;
-
-// Make objects global variables
+// Initialize object pointers
 GtkBuilder *builder;
+
+GtkAdjustment *adjustment_rotation;
+GtkFileFilter *filter;
+
 GtkWidget *window;
-GtkWidget *fixed;
-GtkWidget *label_title;
-GtkWidget *button_import;
-GtkWidget *label_import;
+GtkWidget *box_vertical;
+GtkWidget *box_horizontal;
+
+GtkWidget *image_logo;
 GtkWidget *image;
+GtkWidget *scale_rotation;
+GtkWidget *progress_bar;
+GtkWidget *button_import;
+GtkWidget *button_process;
+
+GtkWidget *label_title;
+GtkWidget *label_import;
+GtkWidget *label_rotation;
+GtkWidget *label_process;
+GtkWidget *label_steps;
+
+GtkWidget *sep_1;
+GtkWidget *sep_2;
+GtkWidget *sep_3;
+GtkWidget *sep_4;
+GtkWidget *sep_5;
+
+GtkWidget *step_0;
+GtkWidget *step_1;
+GtkWidget *step_2;
+GtkWidget *step_3;
+GtkWidget *step_4;
+GtkWidget *step_5;
+GtkWidget *step_6;
+GtkWidget *step_7;
+
 
 int main(int argc, char *argv[]) 
 {
@@ -32,91 +60,52 @@ int main(int argc, char *argv[])
     gtk_builder_connect_signals(builder, NULL);
     
     //Pointers for GTK objects
-    fixed= GTK_WIDGET(gtk_builder_get_object(builder,"fixed"));
-    button_import= GTK_WIDGET(gtk_builder_get_object(builder,"button_import"));
-    label_import= GTK_WIDGET(gtk_builder_get_object(builder, "label_import"));
-    label_title= GTK_WIDGET(gtk_builder_get_object(builder, "label_title"));
-    image= GTK_WIDGET(gtk_builder_get_object(builder,"image"));
+    button_import = GTK_WIDGET(gtk_builder_get_object(builder,"button_import"));
+    label_import = GTK_WIDGET(gtk_builder_get_object(builder, "label_import"));
+    label_title = GTK_WIDGET(gtk_builder_get_object(builder, "label_title"));
+    image = GTK_WIDGET(gtk_builder_get_object(builder,"image"));
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+    box_vertical = GTK_WIDGET(gtk_builder_get_object(builder, "box_vertical"));
+    box_horizontal = GTK_WIDGET(gtk_builder_get_object(builder, "box_horizontal"));
+    image_logo = GTK_WIDGET(gtk_builder_get_object(builder, "image_logo"));
+    scale_rotation = GTK_WIDGET(gtk_builder_get_object(builder, "scale_rotation"));
+    adjustment_rotation = GTK_ADJUSTMENT(gtk_builder_get_object(builder, "adjustment_rotation"));
+    filter = GTK_FILE_FILTER(gtk_builder_get_object(builder, "filter"));
+    progress_bar = GTK_WIDGET(gtk_builder_get_object(builder, "progress_bar"));
+    button_process = GTK_WIDGET(gtk_builder_get_object(builder, "button_process"));
 
+    label_import = GTK_WIDGET(gtk_builder_get_object(builder, "label_import"));
+    label_rotation = GTK_WIDGET(gtk_builder_get_object(builder, "label_rotation"));
+    label_process = GTK_WIDGET(gtk_builder_get_object(builder, "label_process"));
+    label_steps = GTK_WIDGET(gtk_builder_get_object(builder, "label_steps"));
+
+    sep_1 = GTK_WIDGET(gtk_builder_get_object(builder, "sep_1"));
+    sep_2 = GTK_WIDGET(gtk_builder_get_object(builder, "sep_2"));
+    sep_3 = GTK_WIDGET(gtk_builder_get_object(builder, "sep_3"));
+    sep_4 = GTK_WIDGET(gtk_builder_get_object(builder, "sep_4"));
+    sep_5 = GTK_WIDGET(gtk_builder_get_object(builder, "sep_5"));
+
+    step_0 = GTK_WIDGET(gtk_builder_get_object(builder, "step_0"));
+    step_1 = GTK_WIDGET(gtk_builder_get_object(builder, "step_1"));
+    step_2 = GTK_WIDGET(gtk_builder_get_object(builder, "step_2"));
+    step_3 = GTK_WIDGET(gtk_builder_get_object(builder, "step_3"));
+    step_4 = GTK_WIDGET(gtk_builder_get_object(builder, "step_4"));
+    step_5 = GTK_WIDGET(gtk_builder_get_object(builder, "step_5"));
+    step_6 = GTK_WIDGET(gtk_builder_get_object(builder, "step_6"));
+    step_7 = GTK_WIDGET(gtk_builder_get_object(builder, "step_7"));
+    
+    
     gtk_widget_show(window);
     gtk_main();
     return EXIT_SUCCESS;
 }
 
-/*
-void on_button_import_clicked(GtkButton *b)
+void on_button_import_clicked()
 {
     gtk_label_set_text(GTK_LABEL(label_import),(const gchar*)
     "Import image from device files");
-    
-    //GtkWidget *dialog;
-    //dialog = gtk_file_chooser_dialog_new("Import image from device files", GTK_WINDOW(window),
-    //GTK_FILE_CHOOSER_ACTION_OPEN, GTK_STOCK_OK, GTK_RESPONSE_OK, GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL, NULL);
-    //gtk_widget_show_all(dialog);
-    //gint resp = 
-    
-    
     GtkFileChooser *chooser = GTK_FILE_CHOOSER(button_import);
     gchar *filename = gtk_file_chooser_get_filename(chooser);
-    
-    //gchar *filename = gtk_file_chooser_get_filename(button_import("Import image from device files"));
-    //gchar *filename = gtk_file_chooser_get_current_name(button_import);
     gtk_image_set_from_file(GTK_IMAGE(image),filename);
     g_free(filename);
-}
-*/
-
-
-void on_button_import_clicked(GtkButton *button) {
-    GtkWidget *dialog;
-    GtkFileChooser *chooser;
-    GtkFileFilter *filter;
-
-    // Use the main window as the parent for the dialog
-    GtkWidget *parent_window = gtk_widget_get_toplevel(GTK_WIDGET(button));
-
-    // Create a file chooser dialog
-    dialog = gtk_file_chooser_dialog_new("Choose an image", GTK_WINDOW(parent_window),
-                                         GTK_FILE_CHOOSER_ACTION_OPEN, 
-                                         "_Cancel", GTK_RESPONSE_CANCEL, 
-                                         "_Open", GTK_RESPONSE_ACCEPT, NULL);
-    
-    // Check if dialog is created
-    if (dialog == NULL) 
-    {
-        g_print("Failed to create dialog\n");
-        return;
-    }
-
-    chooser = GTK_FILE_CHOOSER(dialog);
-
-    // Add file filters
-    filter = gtk_file_filter_new();
-    gtk_file_filter_add_pattern(filter, "*.jpg");
-    gtk_file_filter_add_pattern(filter, "*.jpeg");
-    gtk_file_filter_add_pattern(filter, "*.png");
-    gtk_file_filter_add_pattern(filter, "*.bmp");
-    gtk_file_chooser_add_filter(chooser, filter);
-
-    // Show the dialog and wait for a response
-    int response = gtk_dialog_run(GTK_DIALOG(dialog));
-    if (response == GTK_RESPONSE_ACCEPT) {
-        // Free the previous file path, if any
-        if (image_file_path) 
-        {
-            g_free(image_file_path);
-        }
-
-        image_file_path = gtk_file_chooser_get_filename(chooser);
-        g_print("Selected file: %s\n", image_file_path);  // Debug output
-        gtk_button_set_label(button, image_file_path);
-        load_image(image_file_path);
-    } 
-    else 
-    {
-        g_print("Dialog closed without selecting a file\n");  // Debug output
-    }
-
-    // Destroy the dialog
-    gtk_widget_destroy(dialog);
 }
