464095255fadcd1cf131df2e191f7f83bb824d5c
Darkrentin <79848980+Darkrentin@users.noreply.github.com>
2024-10-15 20:12:37 +0200

    Grid detection (#54)

--- OCR/src/Pretreatment/GridDetection/Lib/Lib.h
+++ OCR/src/Pretreatment/GridDetection/Lib/Lib.h
@@ -5,6 +5,7 @@
 #include "../Shape/shape.h"
 #include "../List/Node.h"
 
+SDL_Surface* DuplicateSurface(SDL_Surface* surface);
 void MallocMatrix(int ***arr, int sizex, int sizey);
 void FreeMatrix(int **arr, int sizex);
 
@@ -13,7 +14,12 @@ void InitMatrix(SDL_Surface *surface, int*** Map, int*** surf);
 void PrintNodeList(Node* lst, char* name);
 double distance(int x1, int y1, int x2, int y2);
 
+int ListSum(Node* lst);
+Node** ReduceArray(Node** lst, int* size, int nsize);
+void FindShapePosMatrix(Node* shapeList, int* j, int* i);
+
 void Draw(SDL_Surface *surface, Node* s, int r, int g, int b);
+void DrawList(SDL_Surface* surface, Node** clusterList, int size);
 
 #endif
 
