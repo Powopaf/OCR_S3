29f8d6b44d8191d9a84fd5b72358330ac274ca59
Adrien <145380415+tsunooky@users.noreply.github.com>
2024-10-17 08:31:29 +0200

    Refractor interface.c

--- OCR/src/Gtk/interface.c
+++ OCR/src/Gtk/interface.c
@@ -4,6 +4,10 @@
 #include "../../src/Pretreatment/rotation/rotate.h"
 #include <gtk/gtk.h>
 
+// Global variables
+size_t CurrentState = 0;
+int height = 1080;
+
 // Initialize object pointers
 GtkBuilder *builder;
 GtkAdjustment *adjustment_rotation;
@@ -39,31 +43,8 @@ GtkToggleButton *step_8;
 GtkWidget *image;
 GtkWidget *imageTMP;
 
-size_t CurrentState = 0;
-int height = 1080;
-
-int main(int argc, char *argv[]) 
+void get_gtk_widgets()
 {
-    // Initialize GTK
-    gtk_disable_setlocale();
-    gtk_init(&argc, &argv);
-    
-    // Reads the XML glade file
-    builder = gtk_builder_new_from_file("src/Gtk/interface.glade");
-    
-    // Builds the GTK window
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
-    
-    // Set the window title
-    gtk_window_set_title(GTK_WINDOW(window), "CLAPS Word Search OCR");
-    
-    // Destroys program on application exit
-    g_signal_connect(window, "destroy", G_CALLBACK(on_window_destroy), NULL);
-    
-    // Builds a table to see where does the signals come from
-    gtk_builder_connect_signals(builder, NULL);
-    
-    // Pointers for GTK objects
     button_import = GTK_WIDGET(gtk_builder_get_object(builder,"button_import"));
     label_import = GTK_WIDGET(gtk_builder_get_object(builder, "label_import"));
     label_title = GTK_WIDGET(gtk_builder_get_object(builder, "label_title"));
@@ -77,7 +58,6 @@ int main(int argc, char *argv[])
     filter = GTK_FILE_FILTER(gtk_builder_get_object(builder, "filter"));
     button_process = GTK_WIDGET(gtk_builder_get_object(builder, "button_process"));
     button_export = GTK_WIDGET(gtk_builder_get_object(builder, "button_export"));
-    label_import = GTK_WIDGET(gtk_builder_get_object(builder, "label_import"));
     label_rotation = GTK_WIDGET(gtk_builder_get_object(builder, "label_rotation"));
     label_process = GTK_WIDGET(gtk_builder_get_object(builder, "label_process"));
     label_steps = GTK_WIDGET(gtk_builder_get_object(builder, "label_steps"));
@@ -96,11 +76,35 @@ int main(int argc, char *argv[])
     step_6 = GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder, "step_6"));
     step_7 = GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder, "step_7"));
     step_8 = GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder, "step_8"));
-    
+}
+
+int main(int argc, char *argv[]) 
+{
+    // Initialize GTK
+    gtk_disable_setlocale();
+    gtk_init(&argc, &argv);
+
+    // Reads the XML glade file
+    builder = gtk_builder_new_from_file("src/Gtk/interface.glade");
+
+    // Builds the GTK window
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+    gtk_window_set_title(GTK_WINDOW(window), "CLAPS Word Search OCR");
+
+    // Modify height on window resize
     g_signal_connect(window, "configure-event", G_CALLBACK(on_window_resize), NULL);
+
+    // Destroys program on application exit
+    g_signal_connect(window, "destroy", G_CALLBACK(on_window_destroy), NULL);
+
+    // Builds a table to see where does the signals come from
+    gtk_builder_connect_signals(builder, NULL);
     
+    // Building GTK objects
+    get_gtk_widgets();
+
     imageTMP = NULL;
-    
+
     gtk_widget_show(window);
     gtk_main();
     g_object_unref(builder);
