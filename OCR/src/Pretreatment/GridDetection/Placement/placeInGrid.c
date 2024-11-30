a6b44fafa4a2d2f3bd38032892f10a8bd8aedfa1
pierre-antoine.fournier <pierre-antoine.fournier@epita.fr>
2024-11-30 17:24:14 +0100

    test

--- OCR/src/Pretreatment/GridDetection/Placement/placeInGrid.c
+++ OCR/src/Pretreatment/GridDetection/Placement/placeInGrid.c
@@ -7,13 +7,5 @@ Shape** generateMatrix(const Node* ShapeList, size_t* sizeX, size_t* sizeY)
     *   the two pointer are modified in the function
     *   ShapeList is a cluster thar represent the grid
     */
-    if (ShapeList == NULL) { err(EXIT_FAILURE, "Shapelist point to NULL\n"); }
-   
-    Shape** res = malloc(sizeof(struct Shape*) * LenNode(&ShapeList));
-    
-    while (ShapeList != NULL)
-    {
-        ShapeList = ShapeList->next;
-    }
-    return NULL;
+    // TODO
 }
