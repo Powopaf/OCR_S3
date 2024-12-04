e34f41f19323f83c050aee3005e420c1bb31f996
pierre-antoine.fournier <pierre-antoine.fournier@epita.fr>
2024-12-04 12:33:58 +0100

    finish testing sorting algorithm

--- OCR/src/Pretreatment/GridDetection/Placement/placeInGrid.c
+++ OCR/src/Pretreatment/GridDetection/Placement/placeInGrid.c
@@ -1,22 +1,22 @@
 #include "placeInGrid.h"
 #include <err.h>
 
-void swap(struct Shape* p1, struct Shape* p2) {
+void swap(struct Node* p1, struct Node* p2) {
 	/*
 	 * list -> linked list
 	 * we swap p1 and p2 !! p1 > p2
 	 * p1, p2 index of the values to switch
 	 */
-	struct Shape t = *p1;
-	*p1 = *p2;
-	*p2 = t;
+	struct Shape* t = p1->data;
+	p1->data = p2->data;
+	p2->data = t;
 }
 
 void sort_cluster(struct Node* lst) {
 	for (struct Node* i = lst; i != NULL; i = i->next) {
 		for (struct Node* j = i->next; j != NULL; j = j->next) {
 			if (j->data->Cx < i->data->Cx) {
-				swap(j->data, i->data);
+				swap(j, i);
 			}
 		}
 	}
