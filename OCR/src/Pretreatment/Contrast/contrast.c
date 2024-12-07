bef4b07a7af384f667fdca40bb406424557b7605
pierre-antoine.fournier <pierre-antoine.fournier@epita.fr>
2024-12-07 10:53:13 +0100

    new contrast

--- OCR/src/Pretreatment/Contrast/contrast.c
+++ OCR/src/Pretreatment/Contrast/contrast.c
@@ -1,42 +1,35 @@
 #include <SDL2/SDL.h>
+#include <err.h>
 
-void contrast(SDL_Surface* surface) {
-    // init image loop
-    int width = surface->w;
-    int height = surface->h;
-    int bpp = surface->format->BytesPerPixel;
-    SDL_LockSurface(surface);
-    
-    for (int i = 1; i < height - 1; i++) {
-     
-        for (int j = 1; j < width - 1; j++) {
-            //current pixel
-            Uint8* pixel = (Uint8*)surface->pixels + i * surface->pitch + j * bpp;
-            Uint8 side_pixel[8];
-            // we only look at the red color since r=g=b then we modify the r
-            // value
-            side_pixel[0] = *(pixel - width * bpp - 3);    // 0 1 2
-            side_pixel[1] = *(pixel - width * bpp);        // 3 c 4
-            side_pixel[2] = *(pixel - width * bpp + 3);    // 5 6 7
-            side_pixel[3] = *(pixel - 3);                  // [11,12,13] [21,22,23] [31,32,33]
-            side_pixel[4] = *(pixel + 3);                  // [11,12,13] [21,22,23] [31,32,33]
-            side_pixel[5] = *(pixel + width * bpp - 3);    // [11,12,13] [21,22,23] [31,32,33]
-            side_pixel[6] = *(pixel + width * bpp);
-            side_pixel[7] = *(pixel + width * bpp + 3);
-            for (size_t k = 0; k < 8; k++) {
-                if (side_pixel[k] < pixel[0] && pixel[0] < 250) {
-                    pixel[2] = 0;//pixel[2] - 100 < 0 ? 0 : pixel[2] - 100;
-                }
-            }
-        }
-    }
-    // set r to the g and b value
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            Uint8* pixel = (Uint8*)surface->pixels + i * surface->pitch + j * bpp;
-            pixel[0] = pixel[2];
-            pixel[1] = pixel[2];
-        }
-    }
-    SDL_UnlockSurface(surface);
+void MinMaxPixel(SDL_Surface* img, Uint8 res[2]) {
+
+	for (int i = 0; i < img->h; i++) {
+		for (int j = 0; j < img->w; j++) {
+			Uint8* pix = (Uint8*)img->pixels + i * img->pitch + j * img->format->BytesPerPixel;
+			if (pix[0] < res[0]) {
+				res[0] = pix[0];
+			}
+			else if (pix[0] > res[1]) {
+				res[1] = pix[0];
+			}
+		}
+	}
+}
+
+void contrast(SDL_Surface* img) {
+	Uint8 m[2] = { (Uint8)255, (Uint8)255 };
+	MinMaxPixel(img, m);
+	if (m[0] == m[1]) {
+		return;
+	}
+	for (int i = 0; i < img->h; i++) {
+		for (int j = 0; j < img->w; j++) {
+			Uint8* pix = (Uint8*)img->pixels + i * img->pitch + j * img->format->BytesPerPixel;
+			double v = (double)(pix[0] - m[0]) / (m[1] - m[0]) * 255;
+			Uint8 new = (Uint8)v;
+			pix[0] = new;
+			pix[1] = new;
+			pix[2] = new;
+		}
+	}
 }
